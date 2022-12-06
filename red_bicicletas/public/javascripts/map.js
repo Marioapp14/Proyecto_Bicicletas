3b5792fe6ef9be056fe986d88c31e11ec21863d7
Mario <marioalejao.014@gmail.com>
2022-12-06 13:01:11 -0500

    Commit Final Api Bicicletas

--- red_bicicletas/public/javascripts/map.js
+++ red_bicicletas/public/javascripts/map.js
@@ -1,3 +1,4 @@
+
 const tilesProvider = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
 let myMap = L.map('main_map').setView([4.6534649, -74.0836453], 13);
@@ -6,4 +7,16 @@ let myMap = L.map('main_map').setView([4.6534649, -74.0836453], 13);
         maxZoom:18,
     }).addTo(myMap);
 
-    let  marker = L.marker([4.6534649, -74.0836453]).addTo(myMap);
+    let  marker = L.marker([4.6534649, -74.0836453], ).addTo(myMap);
+    let  marker2 = L.marker([4.6187533, -74.1592163], ).addTo(myMap);
+
+    $.ajax({
+        dataType: "json",
+        url: "/api/bicicletas",
+        success: function(result){
+            console.log(result);
+            result.bicicletas.forEach(function(bici){
+                let  marker = L.marker(bici.ubicacion, {title: bici.id}).addTo(myMap);            
+            });
+        }
+    })
